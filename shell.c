c01159ba1ee6872c85dd21330ee6bea0e49f2757
Hanifa-10 <chep.hanifa@gmail.com>
2021-08-14 00:54:35 -0700

    shell

--- shell.c
+++ shell.c
@@ -1,3 +1,12 @@
-#!/bin/sh
-#dhshell$
+#include "shell.h"
+
+/**
+* main - runs a simple UNIX command line interpreter
+* @argc: number of arguments passed to the program
+* @argv: array of pointers to the arguments passed
+* Return: value of the last exeecuted command
+*/
+
+int main(int argc, char *argv[])
+{
 
