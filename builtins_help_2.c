bf54ea5225aad136df188e04ddefe49a19d4144f
Hanifa-10 <84616522+Hanifa-10@users.noreply.github.com>
2021-08-25 12:46:22 +0300

    Update builtins_help_2.c

--- builtins_help_2.c
+++ builtins_help_2.c
@@ -33,3 +33,19 @@ void help_setenv(void)
 	msg = "\tUpon failure, prints a message to stderr.\n";
 	write(STDOUT_FILENO, msg, _strlen(msg));
 }
+
+
+/**
+ * help_unsetenv - Displays information on the shellby builtin command
+ * 'unsetenv'.
+ */
+void help_unsetenv(void)
+{
+	char *msg = "unsetenv: unsetenv [VARIABLE]\n\tRemoves an ";
+
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "environmental variable.\n\n\tUpon failure, prints a ";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "message to stderr.\n";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+}
